59a252d447fd789c2673f77d52003fb93bacc3a9
unknown <002.aureai@gmail.com>
2026-06-05 19:47:49 +0300

    asdmlasşdmsa

--- src/bin/pnass.rs
+++ src/bin/pnass.rs
@@ -1,12 +1,12 @@
-use pandora_toolchain::libkagami::drawing::parse::Drawing;
-use std::str::FromStr;
 use clap::Parser;
+use pandora_toolchain::libkagami::core::{ScriptInfo, SubstationAlpha};
+use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
 #[command(
     name = "pnass",
     version = "0.1.1",
-    about = "Pandora Toolchain ASS tool",
+    about = "Pandora Toolchain ASS standardiser",
     long_about = None
 )]
 struct Args {
@@ -14,12 +14,29 @@ struct Args {
     path: String,
     #[arg(short, long)]
     out: String,
+    #[arg(short, long)]
+    title: String,
 }
-// NOBORE! SUSUME! TAKAI TOU!
+
 #[tokio::main]
 async fn main() {
+    let args = Args::parse();
 
-    let clip = "m 472 708 l 332 406 1224 342 1250 826 b 1055 694 665 431 470 300 558 278 869 564 824 212 985 260 1307 357 1468 406";
-    let drawing = Drawing::from_str(clip).unwrap();
-    println!("{:?}", drawing.commands);
+    let mut sub = SubstationAlpha::load(PathBuf::from(&args.path), false).await;
+
+    sub.script_info = ScriptInfo {
+        title: args.title,
+        script_type: "v4.00+".to_string(),
+        wrap_style: 2,
+        scaled_border_and_shadow: true,
+        playresx: 1920,
+        playresy: 1080,
+        ycbcr_matrix: "TV.709".to_string(),
+        layout_res_x: 1920,
+        layout_res_y: 1080,
+    };
+
+    if let Err(()) = sub.dump_to_file(PathBuf::from(&args.out)).await {
+        eprintln!("pnass: failed to write {}", args.out);
+    }
 }
