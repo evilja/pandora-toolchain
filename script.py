1be17adcb34f423c32729ae1ed070fb622a1fe1a
myisha <002.aureai@gmail.com>
2026-06-23 20:05:00 +0300

    x

--- script.py
+++
@@ -1,9 +0,0 @@
-import os
-import pathlib
-import shutil
-import glob
-
-while True:
-    os.system("cargo clean -p pandora-toolchain")
-    os.system("cargo build --timings")
-    os.system("target\\debug\\pndc.exe")
